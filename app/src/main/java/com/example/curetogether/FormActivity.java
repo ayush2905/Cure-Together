2e8d33f4eefff44834b3ca2be9af09185bc2ef4c
mitrukahitesh <mitrukahitesh@gmail.com>
2021-05-20 16:39:01 +0530

    added jks

--- app/src/main/java/com/example/curetogether/FormActivity.java
+++ app/src/main/java/com/example/curetogether/FormActivity.java
@@ -35,7 +35,7 @@ public class FormActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form);
+        setContentView(R.layout.activity_form); 
         databaseUser = FirebaseDatabase.getInstance().getReference().child("user");
 
         changeStatusBarColor();
